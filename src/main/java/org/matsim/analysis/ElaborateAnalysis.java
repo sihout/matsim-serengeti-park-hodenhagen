9e0cd4e22313c0fbc2beeeb447be956870e7a030
sihout <simon.houterman@tu-berlin.de>
2022-05-16 16:14:56 +0200

    EventHandler completed

--- src/main/java/org/matsim/analysis/ElaborateAnalysis.java
+++ src/main/java/org/matsim/analysis/ElaborateAnalysis.java
@@ -2,12 +2,14 @@ package org.matsim.analysis;
 
 import org.matsim.core.events.EventsUtils;
 
-public class SimpleAnalysis {
+public class ElaborateAnalysis {
     public static void main(String[] args) {
         var handler=new ElaborateEventHandler();
         var manager= EventsUtils.createEventsManager();
         manager.addHandler(handler);
 
         EventsUtils.readEvents(manager,"C:\\Users\\Simon\\IdeaProjects\\matsim-serengeti-park-hodenhagen\\scenarios\\serengeti-park-v1.0\\output\\output-serengeti-park-v1.0-run1\\serengeti-park-v1.0-run1.output_events.xml.gz");
+
+        handler.getCounter().forEach((hour, count) -> System.out.println(hour+"Uhr: "+count));
     }
 }
--- src/main/java/org/matsim/analysis/ElaborateEventHandler.java
+++ src/main/java/org/matsim/analysis/ElaborateEventHandler.java
@@ -3,16 +3,29 @@ package org.matsim.analysis;
 import org.matsim.api.core.v01.events.LinkLeaveEvent;
 import org.matsim.api.core.v01.events.handler.LinkLeaveEventHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ElaborateEventHandler implements LinkLeaveEventHandler {
 
-    private Map<>;
+    private final String intLinkId ="2344590910000f";
 
-    //unfertig
+    public Map<Integer, Integer> getCounter() {
+        return counter;
+    }
+
+    private final Map<Integer, Integer> counter = new HashMap<>();
 
     @Override
     public void handleEvent(LinkLeaveEvent linkLeaveEvent) {
         int hour= (int) (linkLeaveEvent.getTime()/3600);
+
+        if (linkLeaveEvent.getLinkId().toString().equals(intLinkId)){
+            int cnt=1;
+            if (counter.containsKey(hour)){
+                cnt  =counter.get(hour)+1;
+            }
+            counter.put(hour, cnt);
+        }
     }
 }
